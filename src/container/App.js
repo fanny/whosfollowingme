070375b2e234dad765f2ddc6520af4837f7b06d4
Fanny Vieira <fanny.vieira@ccc.ufcg.edu.br>
2018-03-28 15:52:04 -0300

    fixes #2

--- src/container/App.js
+++ src/container/App.js
@@ -7,30 +7,33 @@ class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = { isFollower: false }
+    this.state = { isFollower: false};
   }
 
 
   componentDidMount(){
-    
     axios.get(`https://api.github.com/users/${this.props.toUser}/following/${this.props.currentUser}`)
-      .then(
-        response => this.setState({isFollower: response.status === 204})
-      )
-      .catch(
-        error => this.setState({isFollower: error.response.status !== 404})
-      )
+        .then(
+          response => this.setState({isFollower: response.status === 204})
+        ).catch(
+          error => console.log(error)
+        );
+  
   }
 
 
   render() {
-    const isFollower = this.state.isFollower;
+    const isFollower = this.state.isFollower; 
+    const userProfile = this.props.toUser === this.props.currentUser;
+    let button = '';
 
-    const button = isFollower ? (
-      < UserFollows/>
-    ):(
-      <UserNotFollows />
-    );
+    if(!userProfile){
+      button = (isFollower)? (
+        < UserFollows/>
+      ):(
+        <UserNotFollows />
+      );
+    }
 
     return (
       <div>{button}</div>);
