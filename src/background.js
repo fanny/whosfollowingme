e61d39e6cec01d4b09d8bb5a55ffdd710f7c9cae
Fanny Vieira <fanny.vieira@ccc.ufcg.edu.br>
2018-03-28 15:10:28 -0300

    changes in follow component style

--- src/background.js
+++ src/background.js
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener((res, sender, sendResponse) => {
+    
     chrome.cookies.get({ url: 'https://github.com/', name: 'dotcom_user' }, cookie => {
         sendResponse({currentUser: cookie.value});
-       
     });
+
     return true;
-    
 });
