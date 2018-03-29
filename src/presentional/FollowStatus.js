7ef47db32e55902233e9106a2fcb32207059c52f
Fanny Vieira <fanny.vieira@ccc.ufcg.edu.br>
2018-03-29 12:55:19 -0300

    fixes #4

--- src/presentional/FollowStatus.js
+++ src/presentional/FollowStatus.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './FollowStatus.css'
 
-export const UserFollows = (props) => (<span id='span-follow'>Follow you</span>);
+export const UserFollows = (props) => (<span id='span-follow'>Follows you</span>);
 
-export const UserNotFollows = (props) => (<span id='span-follow'>Doesn't follow you</span>);
+export const UserNotFollows = (props) => (<span id='span-follow'>Doesn't follows you</span>);
